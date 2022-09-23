ae12bc3510231c1b1d9aed075b7fe6698d84a2a5
araratyan98 <araratyanarmen398@gmail.com>
2022-09-23 03:46:06 -0800

    create state

--- src/components/featured/hooks/index.tsx
+++ src/components/featured/hooks/index.tsx
@@ -2,17 +2,19 @@ import Button from '../../shared/button'
 import CreateHooks from '../../shared/create-hooks'
 import NewState from '../../shared/new-state'
 import State from '../../shared/state'
+import StateNew from '../../shared/stateNew'
 import UseCallback from '../../shared/useCallback'
 import UseContext from '../../shared/useContext'
 import UseEffect from '../../shared/useEffect'
 import UseMemo from '../../shared/useMemo'
 import UseRef from '../../shared/useRef'
 import UseState from '../../shared/useState'
+import Wstate from '../../shared/wState'
 
 export default function Hooks() {
   return (
     <div>
-      <UseState />
+      {/* <UseState /> 
       <UseEffect />
       <UseRef />
       <UseMemo />
@@ -21,7 +23,9 @@ export default function Hooks() {
       <CreateHooks />
       <Button />
       <NewState />
-      <State />
+      <State /> */}
+      <Wstate /> 
+      <StateNew />
     </div>
   )
 }
